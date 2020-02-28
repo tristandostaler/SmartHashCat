547e25d9c1822755f58b4b62170f8e61b9d11bfc
tristandostaler <tristandostaler11@hotmail.com>
2020-02-28 15:45:15 -0500

    Update FilterWriteToSmartRule.py

--- SmartHashCat/Filter/FilterWriteToSmartRule.py
+++ SmartHashCat/Filter/FilterWriteToSmartRule.py
@@ -8,14 +8,16 @@ class Filter(FilterAbstract):
     def __init__(self, attacker, previous_input):
         super(Filter, self).__init__(previous_input)
         self.smart_rule = attacker.smart_rule
+        self.has_been_written = False
 
     def get_results(self):
         print("write to smart rule")
-        #for l in self.previous_input.get_results():
-        #    yield l
-        with open(self.smart_rule, 'a') as f:
-            for line in self.previous_input.get_results():
-                #print(line)
-                f.write(f"{line}\n")
-                yield line
-        #return self.previous_input.get_results()
+        for l in self.previous_input.get_results():
+            yield l
+        if not self.has_been_written:
+            with open(self.smart_rule, 'a') as f:
+                for line in self.previous_input.get_results():
+                    #print(line)
+                    f.write(f"{line}\n")
+                    #yield line
+            self.has_been_written = True
