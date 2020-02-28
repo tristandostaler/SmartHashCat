92f40e7966c5c2804a017495fd45e66b25a07d4e
tristandostaler <tristandostaler11@hotmail.com>
2020-02-28 15:44:06 -0500

    Update FilterWriteToSmartFile.py

--- SmartHashCat/Filter/FilterWriteToSmartFile.py
+++ SmartHashCat/Filter/FilterWriteToSmartFile.py
@@ -8,14 +8,16 @@ class Filter(FilterAbstract):
     def __init__(self, attacker, previous_input):
         super(Filter, self).__init__(previous_input)
         self.smart_file = attacker.smart_file
+        self.has_been_written = False
 
     def get_results(self):
         print("Write to smart file")
-        #for l in self.previous_input.get_results():
-        #    yield l
-        with open(self.smart_file, 'a') as f:
-            for line in self.previous_input.get_results():
-                #print(line)
-                f.write(f"{line}\n")
-                yield line
-        #return self.previous_input.get_results()
+        for l in self.previous_input.get_results():
+            yield l
+        if not self.has_been_written:
+            with open(self.smart_file, 'a') as f:
+                for line in self.previous_input.get_results():
+                    #print(line)
+                    f.write(f"{line}\n")
+                    #yield line
+            self.has_been_written = True
